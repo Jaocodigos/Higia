4d3611590a73dbab043aac4978797117d70b43c9
Jaocodigos <jovicode@gmail.com>
2023-07-11 16:44:40 -0300

    fixed broken user authentication

--- docker/engine/app/services/authentication.py
+++ docker/engine/app/services/authentication.py
@@ -24,16 +24,17 @@ def api_auth(roles=[]):
             if auth and hasattr(auth, 'username') and hasattr(auth, 'password'):
                 if admin_username == auth.username and admin_password == auth.password:
                     return func(*args, **kwargs)
-            if request.headers.get('identifier') and request.headers.get('password'):
-                user_id = convert_identifier(request.args.get('identifier'))
-                passwd = request.headers.get('password')
-                user = Users.query.filter_by(identifier=user_id).first()
-                if user and user.check_password(passwd):
-                    if roles and user.check_roles(roles):
-                        return func(*args, **kwargs)
-                    if not roles:
-                        return func(*args, **kwargs)
-                    abort(401, "Unauthorized.")
+                else:
+                    user_identifier = convert_identifier(request.args.get('username'))
+                    passwd = request.headers.get('password')
+                    user = Users.query.filter_by(identifier=user_identifier).first()
+                    if user and user.check_password(passwd):
+                        if roles and user.check_roles(roles):
+                            session['user_id'] = user.id
+                            return func(*args, **kwargs)
+                        if not roles:
+                            return func(*args, **kwargs)
+                        abort(401, "Unauthorized.")
             if 'client-id' in request.headers and 'client' in roles:
                 ##  TODO authentication for client with api-key
                 abort(401, "Unauthorized.")
