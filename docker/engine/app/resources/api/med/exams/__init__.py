fc6d97a4d8916ae55d90c03b71450ac102815c30
Jaocodigos <jovicode@gmail.com>
2023-08-30 18:19:57 -0300

    removed session with user id

--- docker/engine/app/resources/api/med/exams/__init__.py
+++ docker/engine/app/resources/api/med/exams/__init__.py
@@ -2,21 +2,20 @@ from engine.app.resources.api import api
 from engine.app.services.authentication.auth_decorators import api_auth
 from engine.app.models.intern.exams import Exams
 from engine.app.models.intern.users import Users
-from flask import request, jsonify, session
+from flask import request, jsonify
 from engine.app.utils.converters.convert_data_to_model import convert_json_to_model
 from engine.app.utils.converters.convert_user_datas import convert_identifier
 from engine.app.schemas.exams.exam_schema import ExamSchema, ExamUpdateSchema
 import logging
 
-log = logging.getLogger("Higia." + __name__)
+log = logging.getLogger("Higia" + __name__)
 
 
-@api.get('/exams')
+@api.get('/exams/<string:identifier>')
 @api_auth(roles=['doctor'])
-def list_exams():
+def list_exams(identifier):
     log.info('Retrieving exams registered by doctor.')
-    doctor_id = session.get('user_id')
-    doctor = Users.query.filter_by(id=doctor_id).first_or_404()
+    doctor = Users.query.filter_by(identifier=identifier).first_or_404()
     log.debug(f'Returning exams: {doctor.exams}')
     return jsonify({'Exams': [x.serialized for x in doctor.exams]}), 200
 
@@ -37,7 +36,8 @@ def edit_exam(exam_id):
     log.info(f'Altering exam: {exam_id}.')
     exam = Exams.query.filter_by(id=exam_id).first_or_404()
     data = request.json
-    updated_exam = convert_json_to_model(exam, ExamUpdateSchema(), data, converters={'patient_identifier': convert_identifier})
+    updated_exam = convert_json_to_model(exam, ExamUpdateSchema(), data,
+                                         converters={'patient_identifier': convert_identifier})
     log.debug(f'Exam data altered!')
     return updated_exam, 200
 
