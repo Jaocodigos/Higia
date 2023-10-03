181b8526baa74c26384d5c55253b7ccf3ce2b369
Jaocodigos <jovicode@gmail.com>
2023-10-02 22:16:21 -0300

    changed user-model lazy to select for better performance

--- docker/engine/app/models/intern/users.py
+++ docker/engine/app/models/intern/users.py
@@ -11,7 +11,7 @@ class Users(DefaultModel, db.Model):
     email = db.Column(db.String(255), unique=True, nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(500))
+    password_hash = db.Column(db.String(500))
     phone = db.Column(db.String(13), unique=True)
     cep = db.Column(db.String(255))
     last_login = db.Column(db.DateTime, default=datetime.now())
@@ -20,16 +20,16 @@ class Users(DefaultModel, db.Model):
     blocked_until = db.Column(db.DateTime)
     login_tries = db.Column(db.Integer, default=0)
     roles = db.Column(db.Text(20000), default='patient')
-    patient_exams = db.relationship('Exams', lazy='dynamic', foreign_keys='Exams.patient')
-    doctor_exams = db.relationship('Exams', lazy='dynamic', foreign_keys='Exams.doctor')
-    patient_schedulers = db.relationship('Scheduling', lazy='dynamic', foreign_keys='Scheduling.patient')
-    doctor_schedulers = db.relationship('Scheduling', lazy='dynamic', foreign_keys='Scheduling.doctor')
+    patient_exams = db.relationship('Exams', lazy='select', foreign_keys='Exams.patient')
+    doctor_exams = db.relationship('Exams', lazy='select', foreign_keys='Exams.doctor')
+    patient_schedulers = db.relationship('Scheduling', lazy='select', foreign_keys='Scheduling.patient')
+    doctor_schedulers = db.relationship('Scheduling', lazy='select', foreign_keys='Scheduling.doctor')
 
     @property
     def serialized(self):
         return dict(
 
-            name=f"{self.first_name} {self.last_name}",
+            name=f"{self.user_name}",
             cpf=self.identifier,
             email=self.email,
             phone=self.phone,
@@ -37,15 +37,24 @@ class Users(DefaultModel, db.Model):
             roles=self.roles
         )
 
-    def set_hash_password(self, password):
-        self.password = generate_password_hash(password)
+    @property
+    def password(self):
+        return None
+
+    @password.setter
+    def password(self, password_data):
+        self.password_hash = generate_password_hash(password_data)
 
     def check_authorization(self, password):
-        if check_password_hash(self.password, password):
+        if check_password_hash(self.password_hash, password):
             return True
         return False
 
     def check_roles(self, roles: list):
-        if len(roles) > 0 and any(x in roles for x in self.roles):
+        if len(roles) > 0 and any(x in self.roles for x in roles):
             return True
         return False
+
+    @property
+    def user_name(self):
+        return f"{self.first_name} {self.last_name}"
