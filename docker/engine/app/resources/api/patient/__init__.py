7b858b318395fa785a4d143d2c02e73598bc2112
Jaocodigos <jovicode@gmail.com>
2023-10-24 17:23:19 -0300

    [WIP] #11

---
+++ docker/engine/app/resources/api/patient/__init__.py
@@ -0,0 +1,2 @@
+from engine.app.resources.api.patient.scheduling import *
+from engine.app.resources.api.patient.exams import *
--- docker/engine/app/resources/api/patient/scheduling/__init__.py
+++ docker/engine/app/resources/api/patient/scheduling/__init__.py
@@ -1,8 +1,10 @@
 from engine.app.resources.api import api
 from engine.app.services.authentication.auth_decorators import api_auth
 from engine.app.models.intern.users import Users
+from engine.app.models.intern.scheduling import Scheduling
 from flask import request, jsonify
 from engine.app.utils.converters.convert_data_to_model import convert_json_to_model
+from engine.app.utils.converters.convert_dates import convert_string_date_to_datetime
 from engine.app.schemas.schedulers.schedule_schema import ScheduleSchema
 import logging
 
@@ -12,35 +14,26 @@ log = logging.getLogger("Higia." + __name__)
 @api.get('/patient/schedules')
 @api_auth(roles=['patient'])
 def show_schedules(patient):
-    log.info(f'Retrieving doctor {patient.username} schedulers.')
+    log.info(f'Retrieving patient {patient.username} schedulers.')
     patient = Users.query.filter_by(identifier=patient.identifier).first_or_404()
     schedulers = patient.patient_schedulers
     log.debug(f'Schedulers: {len(schedulers)}')
     return jsonify({'Schedules': [x.serialized(x.protected_fields) for x in schedulers]}), 200
 
 
-@api.post('/patient/schedules')
+@api.post('/patient/schedules/<string:doctor_id>')
 @api_auth(roles=['patient'])
-def mark_schedule(patient):
+def mark_schedule(patient, doctor_id):
     log.info(f'Creating a schedule for user with identifier: {patient.identifier}.')
-    doctor = Users.query.filter_by(identifier=patient.identifier).first_or_404()
+    doctor = Users.query.filter_by(id=doctor_id).with_entities(Users.full_name).first_or_404()
+    patient_data = Users.query.filter_by(identifier=patient.identifier).with_entities(Users.id).first_or_404()
     data = request.json
-    schedule = convert_json_to_model(doctor, ScheduleSchema(), data)
+    data['doctor_name'] = doctor.full_name
+    data['doctor'] = doctor_id
+    data['patient'] = patient_data.id
+    data['patient_name'] = patient.username
+    data['patient_identifier'] = patient.identifier
+    schedule = convert_json_to_model(Scheduling(), ScheduleSchema(), data,
+                                     converters={'appointment_day': convert_string_date_to_datetime})
     log.debug(f'Schedule data altered!')
     return schedule, 201
-
-
-@api.delete('/patient/schedules/<string:schedule_id>')
-@api_auth(roles=['patient'])
-def markoff_schedule(patient, schedule_id):
-    log.info(f'Deleting schedule of user {patient.identifier}.')
-    patient = Users.query.filter_by(identifier=patient.identifier).first_or_404()
-    log.debug(f'Deleting patient schedule with identifier: {patient.identifier}')
-    try:
-        schedule = list(filter(lambda x: x.id == schedule_id, patient.patient_schedulers))[0]
-        schedule.delete()
-    except Exception as e:
-        log.error(f"Error while finding and deleting schedule of user {patient.identifier}: {e}")
-        return jsonify({'Status': 'Conflict!'}), 409
-    log.info("User deleted!")
-    return jsonify({'Status': 'Deleted!'}), 200
